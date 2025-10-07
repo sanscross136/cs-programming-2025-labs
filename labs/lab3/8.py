99b7fa05a749ee7fe85a16517cf59ca018a9af71
Сахарюк Александр <sanscross@stud.vvsu.ru>
2025-10-07 12:05:46 +1000

    Update 8.py

--- labs/lab3/8.py
+++ labs/lab3/8.py
@@ -1,7 +1,6 @@
 while(1):
 
-    print('Введите два числа через пробел: ')
-    user_input = input()
-    temp_array = user_input.split(' ')
-    sum = int(temp_array[0]) + int(temp_array[1])
+    print('Введите два числа через пробел: ')  
+    user_input = input().split(' ')
+    sum = int(user_input[0]) + int(user_input[1])
     print(f'Сумма равна: {sum}')
